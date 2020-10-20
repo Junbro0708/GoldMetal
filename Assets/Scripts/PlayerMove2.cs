a9ec555e9aa9d9e3ea970a08e8666451856cb5b2
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-10-20 22:37:57 +0900

    I studied Time.deltaTime in Unity with c#

--- Assets/Scripts/PlayerMove2.cs
+++ Assets/Scripts/PlayerMove2.cs
@@ -19,13 +19,13 @@ public class PlayerMove2 : MonoBehaviour
         if(Input.GetButton("Horizontal"))
         {
             Debug.Log("플레이어가 횡 이동 중..."
-                + Input.GetAxis("Horizontal")); // GetAxis는 반환 타입이 float값이다.
+                + Input.GetAxis("Horizontal") * Time.deltaTime); // GetAxis는 반환 타입이 float값이다.
             //GetAxis가 아닌 GetAxisRaw를 사용하면 가중치가 아닌 -1과 1, 두개 키를 동시에 누르면 0이 나온다.
         }
         if (Input.GetButton("Vertical"))
         {
             Debug.Log("플레이어가 종 이동 중..."
-                + Input.GetAxisRaw("Vertical")); // GetAxis는 반환 타입이 float값이다.
+                + Input.GetAxisRaw("Vertical") * Time.deltaTime); // GetAxis는 반환 타입이 float값이다.
             //GetAxis가 아닌 GetAxisRaw를 사용하면 가중치가 아닌 -1과 1, 두개 키를 동시에 누르면 0이 나온다.
         }
     }
--- Assets/Scripts/RealMove.cs
+++ Assets/Scripts/RealMove.cs
@@ -20,7 +20,9 @@ public class RealMove : MonoBehaviour
     void Update()
     {
         transform.Translate(
-            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Horizontal") * Time.deltaTime, // Time.deltaTime은 컴퓨터의 성능에 상관없이 일정하게 움직이도록 만들어주는 소스
+            // Time.deltaTime : 이전 프레임의 완료까지 걸린 시간
+            // Time.deltaTime은 Translate에서는 벡터에 곱해주고, Vector3에서는 시간에 곱해준다.
             Input.GetAxis("Vertical"), 0);
     }
 }
