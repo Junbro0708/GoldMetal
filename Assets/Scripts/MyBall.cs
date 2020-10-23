0a311710c32ae014f13526f828c824ebacc8832a
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-10-23 23:56:18 +0900

    I studied Rigidbody in Scripts with c#

---
+++ Assets/Scripts/MyBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>(); // Rogidbody의 요소를 다져오는 줄 - 컴포넌트를 사용가능해진다.
+        rigid.velocity = Vector3.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
