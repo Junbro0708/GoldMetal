d4afd15f5ba67ad25084da0d2d93dbe923018ef9
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-10-24 23:52:12 +0900

    I studied Rigidbody in Scripts with c#

--- Assets/Scripts/MyBall.cs
+++ Assets/Scripts/MyBall.cs
@@ -10,7 +10,9 @@ public class MyBall : MonoBehaviour
     void Start()
     {
         rigid = GetComponent<Rigidbody>(); // Rogidbody의 요소를 다져오는 줄 - 컴포넌트를 사용가능해진다.
-        rigid.velocity = Vector3.right;
+        // rigid.velocity = Vector3.right; 오른쪽으로 방향을 주는 코드
+        // rigid.velocity = new Vector3(3, 4, 2); // 만약 계속 속도를 주고 싶다면 업데이트가 아닌 픽스트 업데이트에 써야 한다.
+        rigid.AddForce(Vector3.up * 50, ForceMode.Impulse); // 벡터 방향과 힘, 그리고 힘의 형식을 부여할 수 있다. 보통 캐릭터의 점프에 사용
     }
 
     // Update is called once per frame
