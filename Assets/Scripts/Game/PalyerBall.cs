af1d022c4c8a231a6aae36086316d95c2345158c
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-11-03 00:32:51 +0900

    I started my first game project in Unity with c#

--- Assets/Scripts/Game/PalyerBall.cs
+++ Assets/Scripts/Game/PalyerBall.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PalyerBall : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody rigid;
+    public float jumpPower = 10;
+    bool isJump;
+
     void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>(); // 유니티의 컴포넌트를 스크립트에 가져옴
+        isJump = false;
     }
 
-    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+
     void FixedUpdate()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse); // 기본적인 이동 설정
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Floor")
+        {
+            isJump = false;
+        }
     }
 }
