b85c20d9c12b9cb0c50c5278d3fc8b0d2da9548c
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-11-25 17:14:40 +0900

    I studied Quater View Game using Unity with c#

--- Assets/5Scripts/Quad Game/Player.cs
+++ Assets/5Scripts/Quad Game/Player.cs
@@ -7,11 +7,13 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     public float moveSpeed = 0.5f;
+    public float jumpPower = 1f;
     Vector3 moveVec;
     Animator anim;
     Rigidbody rigid;
     bool wDown;
     bool jDown;
+    bool isJump = false;
 
     void Awake()
     {
@@ -26,6 +28,13 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Jump();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJump = false;
+        }
     }
 
     void GetInput()
@@ -50,9 +59,10 @@ public class Player : MonoBehaviour
     } // 캐릭터 회전 함수
     void Jump()
     {
-        if (jDown)
+        if (jDown && !isJump)
         {
-            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            isJump = true;
         }
     }
 }
